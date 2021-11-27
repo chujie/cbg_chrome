85728dabf2f8ce6c3fac4fd3398f4090a2ae2a92
Jie Chu <umbrella.j@gmail.com>
2021-11-26 23:56:35 -0500

    Packer script: auto version number extraction.

--- pack.sh
+++ pack.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+function usage() {
+    echo "Usage: $0 [version_number]"
+}
 
 if [ "$1" != "" ]; then
-    cd src
-    filename="$1.zip"
-    echo "Packing for version $1"
-    zip -r ../release/$filename . -x "*.DS_Store"
+    PACKAGE_VERSION=$1
 else
-    echo "Usage: $0 version_number"
+    PACKAGE_VERSION=v$(cat src/manifest.json \
+    | grep version \
+    | grep -v manifest \
+    | awk -F: '{print $2}' \
+    | sed 's/[ ",]//g')
 fi
+
+filename="$PACKAGE_VERSION.zip"
+echo "Packing for version $PACKAGE_VERSION"
+OUTPUT_DIR='release'
+if [ ! -d $OUTPUT_DIR ]; then
+    mkdir -p $OUTPUT_DIR
+fi
+zip -r $OUTPUT_DIR/$filename ./src -x "*.DS_Store"
+cp $OUTPUT_DIR/$filename $OUTPUT_DIR/latest.zip
